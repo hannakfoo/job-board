cb3ce66d27ae2518480014c906db2c1dcd8d5a1b
Henk Jan Pluim <henkjan.pluim@greenchoice.nl>
2023-02-10 14:21:10 +0100

    added about page excercise

---
+++ src/pages/about.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from "next";
+
+const AboutPage: NextPage = () => {
+    return (
+        <>
+            <header>
+                <h1> My Job Board</h1>
+               </header>
+            <main><h2>About</h2><p>This job board is the project created using the book</p>
+                <em>Serverless Web Applications with React and Nextjs</em>
+            </main>
+
+
+        </>
+    )
+}
+
+export default AboutPage
